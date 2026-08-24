2e07be787baf211359a805244eae28445985ae4d
web-padawan <iamkulykov@gmail.com>
2026-08-24 13:12:01 +0300

    fix: update copyright year in Lumo task

--- tasks/lumo.js
+++ tasks/lumo.js
@@ -12,7 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 function createCopyright() {
   return `/**
  * @license
- * Copyright (c) 2017 - 2025 Vaadin Ltd.
+ * Copyright (c) 2017 - ${new Date().getFullYear()} Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */`;
 }
